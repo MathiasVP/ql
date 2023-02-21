cbce5631bbb6302036d792365579771200437153
Mathias Vorreiter Pedersen <mathiasvp@github.com>
2023-02-21 11:48:37 +0000

    C++: Fix a bug in Expr <-> Node mapping.

--- cpp/ql/test/library-tests/dataflow/taint-tests/taint.cpp
+++ cpp/ql/test/library-tests/dataflow/taint-tests/taint.cpp
@@ -651,7 +651,7 @@ void test__strnextc(const char* source) {
 		sink(c); // $ ast,ir
 	} while(c != '\0');
 	c = _strnextc("");
-	sink(c); // $ SPURIOUS: ir
+	sink(c);
 }
 
 // --- taint through const specified function ---
